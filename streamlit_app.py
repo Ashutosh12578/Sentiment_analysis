c1a0af51e15551af4c24c46374249474cadf3970
Ashutosh12578 <ashutoshshrotriya91@gmail.com>
2025-05-06 20:39:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,12 +14,12 @@ st.write('This app is for sentiment analysis of social media data !')
 st.sidebar.title('Sentiment analysis of Social Media Data')
 with st.expander('Twitter Data'):
   st.write('**Raw data**')
-  df1=pd.read_csv('https://raw.githubusercontent.com/Wolverine-max/Sentiment-analysis-of-social-media-data/refs/heads/master/Twitter_Data.csv')
+  df1=pd.read_csv('Twitter_Data.csv')
   df1.columns=['text','labels']
   st.dataframe(df1)
 with st.expander('Reddit Data'):
   st.write('**Raw data**')
-  df2=pd.read_csv('https://raw.githubusercontent.com/Wolverine-max/Sentiment-analysis-of-social-media-data/refs/heads/master/Reddit_Data.csv')
+  df2=pd.read_csv('Reddit_Data.csv')
   df2.columns=['text','labels']
   st.dataframe(df2)
 with st.expander('Merged Data'):
